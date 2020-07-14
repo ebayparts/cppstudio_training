ff3ff1d058d4931049b85eb97a69f92664daca81
Ebayparts <sanyav89@ukr.net>
2020-07-14 15:38:11 +0300

    metre into kilometre

---
+++ level0/meters_into_kilometers/meters_into_kilometers/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	double metre;
+	cout << "Enter your distance in Metre to convert in Kilometre:" << endl;
+	cin >> metre;
+	cout << "your distance in Kilometre is: " << metre / 1000 << endl;
+
+	return 0;
+}
