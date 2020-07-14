1531be2271fc348a21b2cecb7ec4fc09f54b080f
Ebayparts <sanyav89@ukr.net>
2020-07-14 16:07:53 +0300

    paint house level0

---
+++ level0/paint_house level0/paint_house level0/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+cout << "\t               /\\                 "<<endl;
+cout << "\t              /  \\                "<<endl;
+cout << "\t             /    \\               "<<endl;
+cout << "\t            /      \\              "<<endl;
+cout << "\t           /        \\             "<<endl;
+cout << "\t          /          \\            "<<endl;
+cout << "\t         /            \\           "<<endl;
+cout << "\t        /              \\          "<<endl;
+cout << "\t       /                \\         "<<endl;
+cout << "\t      /                  \\        "<<endl;
+cout << "\t     /                    \\       "<<endl;
+cout << "\t    /                      \\      "<<endl;
+cout << "\t   /                        \\     "<<endl;
+cout << "\t  /                          \\    "<<endl;
+cout << "\t /                            \\   "<<endl;
+cout << "\t/                              \\  "<<endl;
+cout << "\t________________________________   "<<endl;
+cout << "\t     |                   |         "<<endl;
+cout << "\t     |                   |         "<<endl;
+cout << "\t     |     _________     |         "<<endl;
+cout << "\t     |    |         |    |         "<<endl;
+cout << "\t     |    |         |    |         "<<endl;
+cout << "\t     |    |         |    |         "<<endl;
+cout << "\t     |    |         |    |         "<<endl;
+cout << "\t     |    |         |    |         "<<endl;
+cout << "\t     |    |_________|    |		  "<<endl;
+cout << "\t     |                   |         "<<endl;
+cout << "\t     |                   |         "<<endl;
+cout << "\t     |                   |         "<<endl;
+cout << "\t     _____________________         "<<endl;
+	return 0;
+}
