1d5cb1ad4f74f0ca82aa50e59bad36221d7be641
Ebayparts <sanyav89@ukr.net>
2020-07-14 16:36:23 +0300

    rectangular triangle level0

---
+++ level0/rectangular_triangle level0/rectangular_triangle level0/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Rectangular triangle:" << endl;
+	cout << "                         +" << endl;
+	cout << "                        ++" << endl;
+	cout << "                       +++" << endl;
+	cout << "                      ++++" << endl;
+	cout << "                     +++++" << endl;
+	cout << "                    ++++++" << endl;
+	cout << "                   +++++++" << endl;
+	cout << "                  ++++++++" << endl;
+	cout << "                 +++++++++" << endl;
+	cout << "                ++++++++++" << endl;
+	cout << "               +++++++++++" << endl;
+	cout << "              ++++++++++++" << endl;
+	cout << "             +++++++++++++" << endl;
+	cout << "            ++++++++++++++" << endl;
+	cout << "           +++++++++++++++" << endl;
+	cout << "          ++++++++++++++++" << endl;
+	cout << "         +++++++++++++++++" << endl;
+	cout << "        ++++++++++++++++++" << endl;
+	cout << "       +++++++++++++++++++" << endl;
+	cout << "      ++++++++++++++++++++" << endl;
+	cout << "     +++++++++++++++++++++" << endl;
+	cout << "    ++++++++++++++++++++++" << endl;
+	cout << "   +++++++++++++++++++++++" << endl;
+	cout << "  ++++++++++++++++++++++++" << endl;
+	cout << " +++++++++++++++++++++++++" << endl;
+	cout << "++++++++++++++++++++++++++" << endl;
+
+	return 0;
+}
